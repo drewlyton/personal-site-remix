acd6d315b5eae6074369f0b6ddbae9966610b1f3
Drew Lytle <aml3ed@virginia.edu>
2022-11-10 15:02:09 -0500

    Add rss feed

---
+++ app/helpers/escapeCharacters.ts
@@ -0,0 +1,12 @@
+export function escapeCdata(s: string) {
+  return s.replace(/\]\]>/g, "]]]]><![CDATA[>");
+}
+
+export function escapeHtml(s: string) {
+  return s
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
---
+++ app/helpers/getHost.server.ts
@@ -0,0 +1,8 @@
+export const getHost = (request: Request) => {
+  const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+  if (!host) {
+    throw new Error("Could not determine domain URL.");
+  }
+  return host;
+};
