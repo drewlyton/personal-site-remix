d9b0af2819de85d1dc401b9b08d6ba322748d506
Drew Lytle <aml3ed@virginia.edu>
2023-08-20 08:18:45 -0400

    Update to CDN client

--- app/data/client.ts
+++ app/data/client.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
 export const client = new GraphQLClient(
-  "https://api-us-east-1.graphcms.com/v2/ckvh09pdp3k4t01xn08841lrh/master"
+  "https://us-east-1.cdn.hygraph.com/content/ckvh09pdp3k4t01xn08841lrh/master"
 );
